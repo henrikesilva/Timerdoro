87da9b79e771d5c64c8e39277ff04e851b49ef9c
henrikesilva <henrikelima.0502@gmail.com>
2023-10-02 17:56:54 -0300

    Adicionada a configuração do eslint e correção dos erros

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,10 +1,10 @@
 // classe criada para ajudar com a tipagem do styled-componentes adicionando os temas padroes(que criamos) no projeto
 
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType {}
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Button } from "./components/Button";
-import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
@@ -15,6 +14,6 @@ export function App() {
       <Button />
 
       <GlobalStyle />
-    </ ThemeProvider>
+    </ThemeProvider>
   )
 }
