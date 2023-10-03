4b7a6111026a738eeef50f0faf06c62475bc337f
henrikesilva <henrikelima.0502@gmail.com>
2023-10-03 19:38:26 -0300

    Estilização inicial da Home

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      {/* path = caminho da rota - element = componente da rota */}
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      {/* Serve para aplicar o layout default(Header) em todas as rotas 'filhas' */}
+      <Route path="/" element={<DefaultLayout />}>
+        {/* path = caminho da rota - element = componente da rota */}
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  ContdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <ContdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </ContdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
